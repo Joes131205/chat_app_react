130dab39d4115355253196e4520a591cc7038d15
Joe Steven <stevenjoe1312@gmail.com>
2024-03-25 13:22:17 +0700

    signUp

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,4 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import app from "../firebase";
+import { useEffect, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
 function Root() {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                navigate("/signup");
+            }
+        });
+    }, []);
     return (
         <div>
             <h1>Chat App</h1>
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,50 +1,101 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import app from "../firebase";
-import { useState } from "react";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+} from "firebase/auth";
 
-const auth = getAuth();
+import app from "../firebase";
+import { useEffect, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
 function SignUp() {
     const [data, setData] = useState({
         username: "",
         email: "",
         password: "",
+        confirmPassword: "",
     });
+    const auth = getAuth();
+    const navigate = useNavigate();
+
+    function signUpUser(e) {
+        e.preventDefault();
+        if (data.password === data.confirmPassword) {
+            const { email, password } = data;
+            createUserWithEmailAndPassword(auth, email, password).then(
+                (userCredential) => {
+                    const user = userCredential.user;
+                }
+            );
+        }
+    }
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                navigate("/");
+            }
+        });
+    }, []);
     return (
-        <div>
+        <div className="flex flex-col gap-10 items-center justify-center">
             <h1>Sign Up</h1>
-            <form>
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    onChange={(e) =>
-                        setData({ ...data, username: e.target.value })
-                    }
-                    value={data.username}
-                />
-
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    onChange={(e) =>
-                        setData({ ...data, email: e.target.value })
-                    }
-                    value={data.email}
-                />
-
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    onChange={(e) =>
-                        setData({ ...data, password: e.target.value })
-                    }
-                    value={data.password}
-                />
+            <form
+                className="flex flex-col gap-10 items-center justify-center"
+                onSubmit={signUpUser}
+            >
+                <div className="flex flex-col">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        onChange={(e) =>
+                            setData({ ...data, username: e.target.value })
+                        }
+                        value={data.username}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={(e) =>
+                            setData({ ...data, email: e.target.value })
+                        }
+                        value={data.email}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={(e) =>
+                            setData({ ...data, password: e.target.value })
+                        }
+                        value={data.password}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                confirmPassword: e.target.value,
+                            })
+                        }
+                        value={data.confirmPassword}
+                    />
+                </div>
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
