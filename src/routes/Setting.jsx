08709ce205f6ed691413c094f3e5904b0f65838e
Joe Steven <86814315+Joes131205@users.noreply.github.com>
2024-03-27 13:46:58 +0700

    fix

--- src/routes/Setting.jsx
+++ src/routes/Setting.jsx
@@ -1,23 +1,32 @@
 function Setting() {
-    return ( 
-    <div>
-        <h1>Setting</h1>
+    return (
+        <div>
+            <h1>Setting</h1>
 
-        <form>
-            <div>
-                <label htmlFor="changeUserName">Change Username</label>
-                <input type="text" name="changeUserName" id="changeUserName" />
-            </div>
+            <form>
+                <div>
+                    <label htmlFor="changeUserName">Change Username</label>
+                    <input
+                        type="text"
+                        name="changeUserName"
+                        id="changeUserName"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="changeProfilePicture">Change Profie Picture</label>
-                <input type="file" name="changeProfilePicture" id="changeProfilePicture" />
-            </div>
+                <div>
+                    <label htmlFor="changeProfilePicture">
+                        Change Profie Picture
+                    </label>
+                    <input
+                        type="file"
+                        name="changeProfilePicture"
+                        id="changeProfilePicture"
+                    />
+                </div>
 
-            <input type="submit" value="Submit" />
-
-        </form>
-    </div> 
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
     );
 }
 
