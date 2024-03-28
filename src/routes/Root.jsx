c038778854bd68f9624355585fa025dca33e3227
Joe Steven <stevenjoe1312@gmail.com>
2024-03-28 08:26:56 +0700

    Setting Functionality

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -5,7 +5,7 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import app from "../firebase";
 import { useEffect, useState } from "react";
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import Chat from "../Chat";
 function Root() {
@@ -13,19 +13,19 @@ function Root() {
     const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [profilePicture, setProfilePicture] = useState("");
+
     async function fetchUserName(uid) {
         const db = getFirestore(app);
         const usersCollection = await collection(db, "users");
         const docRef = doc(usersCollection, uid);
         const docSnap = await getDoc(docRef);
         const data = await docSnap.data();
-        console.log(data);
         setUsername(data.username);
     }
 
     async function fetchProfilePicture(uid) {
         const storage = getStorage();
-        const profilePictureRef = await ref(storage, `profile-pictures/${uid}`);
+        const profilePictureRef = ref(storage, `profile-pictures/${uid}.png`);
         const snap = await getDownloadURL(profilePictureRef);
         setProfilePicture(snap);
     }
@@ -40,17 +40,19 @@ function Root() {
             }
         });
     }, []);
+
     return (
         <div>
             <h1>Chat App</h1>
-            <div className="flex gap-2">
+            <div className="flex gap-2 items-center">
                 <img
                     src={profilePicture}
                     alt="Profile Picture"
-                    className="rounded-full w-50 h-50 border-2 border-black"
+                    className="rounded-full w-10 h-10 border-2 border-black"
                 />
                 <p>Hello, {username}!</p>
             </div>
+            <Link to="/setting">Setting</Link>
             <Chat />
             <button onClick={() => signOut(auth)}>Sign Out</button>
         </div>
--- src/routes/Setting.jsx
+++ src/routes/Setting.jsx
@@ -1,31 +1,126 @@
+"use strict";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
 function Setting() {
+    const [data, setData] = useState({
+        changeUserName: "",
+        changeProfilePicture: "",
+        profilePictureReview: "",
+    });
+    const auth = getAuth();
+    const storage = getStorage();
+    const db = getFirestore();
+
+    const navigate = useNavigate();
+
+    async function changeProfilePicture(image) {
+        const storageRef = ref(storage, `profile-pictures/${image.name}`);
+        await uploadBytes(storageRef, image);
+    }
+
+    async function changeUsername(username) {
+        console.log("changing username");
+        try {
+            await setDoc(doc(db, "users", auth.currentUser.uid), {
+                username,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function changeData(e) {
+        const { name, value } = e.target;
+        if (name === "changeProfilePicture") {
+            const file = e.target.files[0];
+            if (file) {
+                // Handles Image Preview
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    setData((prev) => ({
+                        ...prev,
+                        profilePictureReview: reader.result,
+                    }));
+                };
+
+                // Handles Image Naming for Upload
+                const newFilename = `${auth.currentUser.uid}.${file.name
+                    .split(".")
+                    .pop()}`;
+                const newBlob = new Blob([file], {
+                    type: file.type,
+                });
+
+                newBlob.name = newFilename;
+                setData((prev) => ({
+                    ...prev,
+                    changeProfilePicture: newBlob,
+                }));
+                reader.readAsDataURL(file);
+            }
+        } else {
+            setData({ ...data, [name]: value });
+        }
+    }
+
+    async function changeSetting(e) {
+        e.preventDefault();
+        if (!data.changeUserName && !data.changeProfilePicture) {
+            return;
+        }
+        if (data.changeUserName) {
+            await changeUsername(data.changeUserName);
+        }
+        if (data.changeProfilePicture) {
+            await changeProfilePicture(data.changeProfilePicture);
+        }
+        navigate("/");
+    }
+
     return (
         <div>
             <h1>Setting</h1>
 
-            <form>
-                <div>
+            <form
+                className="flex flex-col items-center justify-center gap-10"
+                onSubmit={changeSetting}
+                onChange={changeData}
+            >
+                <div className="flex flex-col gap-2">
                     <label htmlFor="changeUserName">Change Username</label>
                     <input
                         type="text"
                         name="changeUserName"
                         id="changeUserName"
+                        value={data.username}
                     />
                 </div>
 
-                <div>
+                <div className="flex flex-col gap-2">
                     <label htmlFor="changeProfilePicture">
-                        Change Profie Picture
+                        Change Profile Picture (Accepts PNG)
                     </label>
                     <input
                         type="file"
                         name="changeProfilePicture"
                         id="changeProfilePicture"
+                        accept="image/png, image/jpg"
+                    />
+                    <img
+                        src={data.profilePictureReview}
+                        alt="Profile Picture"
+                        className="rounded-full w-24 h-24 border-2 border-black"
                     />
                 </div>
 
                 <input type="submit" value="Submit" />
             </form>
+            <Link to="/">Go back</Link>
         </div>
     );
 }
