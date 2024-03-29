dd100d19510a808838d10a6bed2167db7c3e2e2c
Joe Steven <stevenjoe1312@gmail.com>
2024-03-29 21:58:28 +0700

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 import app from "../firebase";
 import { useState } from "react";
 import { getAuth } from "firebase/auth";
@@ -7,28 +7,30 @@ function Chat() {
     const db = getFirestore(app);
     const auth = getAuth();
     function sendMessage(locale) {
-        const date = new Date();
-        const options = {
-            weekday: "short",
-            year: "numeric",
-            month: "numeric",
-            day: "numeric",
-            hour: "numeric",
-            minute: "2-digit",
-        };
-        const formatter = new Intl.DateTimeFormat(locale, options);
-        const formattedDate = formatter.format(date);
+        if (message) {
+            const date = new Date();
+            const options = {
+                weekday: "short",
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+                hour: "numeric",
+                minute: "2-digit",
+            };
+            const formatter = new Intl.DateTimeFormat(locale, options);
+            const formattedDate = formatter.format(date);
 
-        const uid = auth.currentUser.uid;
-        const newMessageRef = doc(db, "messages", uid);
-        const newMessage = {
-            text: message,
-            createdAt: formattedDate,
-            uid,
-        };
-        setDoc(newMessageRef, newMessage).then(() => {
-            setMessage("");
-        });
+            const uid = auth.currentUser.uid;
+            const newMessageRef = collection(db, "messages");
+            const newMessage = {
+                text: message,
+                createdAt: formattedDate,
+                uid,
+            };
+            addDoc(newMessageRef, newMessage).then(() => {
+                setMessage("");
+            });
+        }
     }
     return (
         <div className="w-full h-[100vh - 1.25rem] margin-0">
@@ -38,6 +40,7 @@ function Chat() {
                 placeholder="Message..."
                 className="h-[2.6rem] w-[90%] border border-black"
                 onChange={(e) => setMessage(e.target.value)}
+                value={message}
             />
             <button
                 className="w-[10%] border border-black h-[2.6rem]"
